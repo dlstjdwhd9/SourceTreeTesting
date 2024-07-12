ec89433f4e13c0c1b711d600aabfda9230c7686f
dlstjdwhd <dlstjdwhd5@naver.com>
2024-07-12 12:02:45 +0900

    - 스크립트 테스트

---
+++ Assets/Scripts/TestScripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("For Testing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
