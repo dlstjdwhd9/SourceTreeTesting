bd94a9082164e5a35805874257070332e67101f5
dlstjdwhd <dlstjdwhd5@naver.com>
2024-08-28 19:55:41 +0900

    TestScript 2

---
+++ Assets/Scripts/TestScripts2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("For Testing");
+        print("For Revert Testing2");
+        print("For Revert Testing3");
+        print("For Revert Testing4");
+        print("For Revert Testing5");
+        print("Token Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
